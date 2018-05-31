f0bcf34abb9f4427e92d795c11c5b1917f916e9c
Jaime Machuca <jaime@element.aero>
2018-05-31 15:44:41 -0500

    Made changes

--- sawr_master/scripts/init_0.sh
+++ sawr_master/scripts/init_0.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Launch phase 0 of the SAWR software stack
 echo "SAWR Phase 0: Motor control and odometry"
-roslaunch sawr_master init_0.launch
+#roslaunch sawr_master init_0.launch
+roslaunch roboclaw_node roboclaw.launch
